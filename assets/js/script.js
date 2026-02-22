3f6808edffa83feb98cbd926512556895ad782af
Gabriel Gurgel <gabrielsilvagurgel@gmail.com>
2026-02-22 20:25:45 -0300

    Muda cor do fundo da secao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,6 @@
 gsap.registerPlugin(TextPlugin, ScrollTrigger, ScrollSmoother);
 
-// animação de flutuar
-gsap.to("#float-img", {
-  y: -20, // sobe 20px
-  duration: 1.5, // tempo de animação
-  ease: "power1.inOut", // suavidade
-  yoyo: true, // voltar
-  repeat: -1, // repetir infinito
-});
-
-gsap.to(".link-conhecer", {
-  "--rotation": "360deg",
-  duration: 2,
-  repeat: -1,
-  ease: "linear"
-});
-
-const btn = document.querySelector(".link-conhecer");
-
-btn.addEventListener("mouseenter", () => {
-  gsap.to(btn, {
-    "--rotation": "360deg",
-    duration: 1.5,
-    repeat: -1,
-    ease: "linear"
-  });
-});
-
-btn.addEventListener("mouseleave", () => {
-  gsap.killTweensOf(btn);
-  gsap.set(btn, { "--rotation": "0deg" });
-});
-
-
+// ANIMAÇÃO OLÁ MUNDO - SEÇÃO HERO:
 const texto = "Olá, mundo!";
 
 const tl = gsap.timeline({ repeat: -1 });
@@ -54,6 +22,24 @@ tl.to("#texto", {
   ease: "none"
 });
 
+// ANIMAÇÃO FLUTUAR MINI COMPUTADOR - SECAO HERO:
+gsap.to("#float-img", {
+  y: -20, // sobe 20px
+  duration: 1.5, // tempo de animação
+  ease: "power1.inOut", // suavidade
+  yoyo: true, // voltar
+  repeat: -1, // repetir infinito
+});
+
+gsap.to(".link-conhecer", {
+  "--rotation": "360deg",
+  duration: 2,
+  repeat: -1,
+  ease: "linear"
+});
+
+
+// ANIMAÇÃO LEGENDAS DE SEÇÃO:
 const legenda = "Porque criar seu site comigo?";
 
 gsap.to("#legend-section", {

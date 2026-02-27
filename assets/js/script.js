05d6193c398e51784a13f25d5b893bcc03a21f66
Gabriel Gurgel <gabrielsilvagurgel@gmail.com>
2026-02-27 20:04:23 -0300

    Faz melhorias de contraste no layout da pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,36 @@
-gsap.registerPlugin(TextPlugin, ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(TextPlugin, ScrollToPlugin, ScrollTrigger, ScrollSmoother);
+
+// SCROLL SUAVE:
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 1.2,
+  effects: true
+});
+
+// SCROLL SUAVE PARA LINKS DENTRO DO SITE:
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const section = document.querySelector(link.getAttribute("href"));
+
+    if (!section) return;
+
+    gsap.to(window, {
+      duration: 1.2,
+      scrollTo: {
+        y: section,
+        offsetY: 80,
+      },
+      ease: "expo.out",
+    });
+  });
+});
 
 // ANIMAÇÃO OLÁ MUNDO - SEÇÃO HERO:
 const texto = "Olá, mundo!";
@@ -6,41 +38,42 @@ const texto = "Olá, mundo!";
 const tl = gsap.timeline({ repeat: -1 });
 
 for (let i = 0; i < 2; i++) {
-  tl.to("#texto", { text: "...", duration: 0.8 })
-    .to("#texto", { duration: 0.4 });
+  tl.to("#texto", { text: "...", duration: 0.8 }).to("#texto", {
+    duration: 0.4,
+  });
 }
 
 tl.to("#texto", {
   duration: texto.length * 0.08,
   text: texto,
-  ease: "none"
+  ease: "none",
 })
-.to("#texto", { duration: 2 })
-.to("#texto", {
-  duration: texto.length * 0.05,
-  text: "",
-  ease: "none"
-});
+  .to("#texto", { duration: 2 })
+  .to("#texto", {
+    duration: texto.length * 0.05,
+    text: "",
+    ease: "none",
+  });
 
 // ANIMAÇÃO FLUTUAR MINI COMPUTADOR - SECAO HERO:
-gsap.fromTo("#float-img",
+gsap.fromTo(
+  "#float-img",
   { y: -15 },
   {
     y: 15,
     duration: 2,
     ease: "sine.inOut",
     repeat: -1,
-    yoyo: true
-  }
+    yoyo: true,
+  },
 );
 
 gsap.to(".link-conhecer", {
   "--rotation": "360deg",
   duration: 2,
   repeat: -1,
-  ease: "linear"
+  ease: "linear",
 });
-
 
 // ANIMAÇÃO LEGENDAS DE SEÇÃO:
 const legenda = "Quem é o GAB?";
@@ -51,5 +84,9 @@ gsap.to("#legend-section", {
   repeat: -1,
   yoyo: true,
   repeatDelay: 2,
-  ease: "none"
+  ease: "none",
 });
+
+
+
+
